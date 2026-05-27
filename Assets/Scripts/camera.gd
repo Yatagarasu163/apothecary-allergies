eff6c89862f0048ff71533a9997675ffc73abbd3
BarrelPanda <samuel.leong.2006@gmail.com>
2026-05-27 18:34:52 +0800

    donez

--- Assets/Scripts/camera.gd
+++ Assets/Scripts/camera.gd
@@ -1,9 +1,16 @@
 extends Camera2D
+class_name CameraControl
 
+@export var camMoveSpeed: float = 15
 @onready var current_item_gui = $TextureRect;
+var currentTargetPosition: Vector2
 
 func _process(_delta: float) -> void:
 	if GameManager.player_inventory == null:
 		current_item_gui.visible = false;
 	else:
 		current_item_gui.visible = true;
+	position = lerp(position, currentTargetPosition, camMoveSpeed * _delta)
+
+func setNewPosition(newPosition: Vector2):
+	currentTargetPosition = newPosition
