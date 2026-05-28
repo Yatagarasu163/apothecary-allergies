a3f96d2f7503a97dd3bc2faf57393283f477103c
limcheexian728-shadyx <cheexianlim96@gmail.com>
2026-05-28 11:52:00 +0800

    Adjust a bit the patient script
    The patient script become more clear and efficiency

--- Assets/Scripts/Patient Scripts/patient.gd
+++ Assets/Scripts/Patient Scripts/patient.gd
@@ -6,12 +6,14 @@ extends Node2D
 @export var patien_status = false
 var interactable : bool = false
 var queueing : bool = false
+var waiting_for_cure : bool = false
 @export var queue_point:Vector2 = Vector2(530, 600);
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	chat_bubble.visible = false
 	label.visible = false
+	position.x = queue_point.x
 
 
 
@@ -20,19 +22,25 @@ func _process(_delta: float) -> void:
 	if(patien_status == false):
 		if(position.y > queue_point.y):
 			position.y -= speed
-		if(position.x != 530):
-			position.x = 530
-	if interactable == true && Input.is_action_just_pressed("Interact"):
-		print("Player took customer's order")
-		label.visible = false
-		chat_bubble.play_symptom_anim();
+	if interactable == true:
+		if Input.is_action_just_pressed("Interact"):
+			if(!waiting_for_cure):
+				label.text = "Press F to take order"
+				print("Player took customer's order")
+				label.visible = false
+				chat_bubble.play_symptom_anim();
+				await get_tree().create_timer(2.0).timeout
+				chat_bubble.visible = false
+				waiting_for_cure = true
+			else:
+				label.text = "Press F to give medicine"
+				serve_medicine();
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("Player") && queueing == false):
 		label.visible = true
 		interactable = true
-		label.text = "Press F to take order"
 	elif(body.is_in_group("Patient")):
 		print("Patient hit patient")
 		speed = 0
@@ -43,3 +51,8 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	if(body.is_in_group("Player") && queueing == false):
 		label.visible = false
 		interactable = false
+	if(body.is_in_group("Patient")):
+		speed = 10
+
+func serve_medicine() -> void:
+	pass
