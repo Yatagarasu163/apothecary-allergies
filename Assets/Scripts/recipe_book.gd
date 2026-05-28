7d7e41bb45773480f0db0ff0cc87365e4fc0a84f
BarrelPanda <samuel.leong.2006@gmail.com>
2026-05-28 12:08:27 +0800

    recipe book

--- Assets/Scripts/recipe_book.gd
+++ Assets/Scripts/recipe_book.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
-var is_player_inside: bool = false;
+@export var recipePages: Array[Control]
+@export var currentPage: int
 
 @onready var recipeUI = $RecipeBookUI;
+
+var is_player_inside: bool = false;
+var newPage: int
 
 func _ready() -> void:
 	recipeUI.visible = false;
@@ -13,8 +17,11 @@ func _process(_delta: float) -> void:
 		recipeUI.global_position = Vector2(0, 0);
 		recipeUI.visible = !recipeUI.visible;
 		print("Showing the recipe!");
-		
-	
+
+func updatePage():
+	recipePages[currentPage].visible = false
+	recipePages[newPage].visible = true
+	currentPage = newPage
 
 func body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
@@ -23,3 +30,15 @@ func body_entered(body: Node2D) -> void:
 func body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		is_player_inside = false;
+
+
+func _on_next_page_pressed() -> void:
+	if (currentPage + 1 > recipePages.size()): return
+	newPage = currentPage + 1
+	updatePage()
+
+
+func _on_prevous_page_pressed() -> void:
+	if (currentPage - 1 < 0): return
+	newPage = currentPage - 1
+	updatePage()
