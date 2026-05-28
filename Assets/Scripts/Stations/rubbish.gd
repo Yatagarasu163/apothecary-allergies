b981571274b18aee7347c0f7d5e8f42cb9761b59
Kay Faraday <yatagarasu163@gmail.com>
2026-05-28 16:29:35 +0800

    Fixed rubbish sprite

--- Assets/Scripts/Stations/rubbish.gd
+++ Assets/Scripts/Stations/rubbish.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 
 var is_player_inside: bool = false;
+@onready var anim: AnimatedSprite2D = $"Rubbish Sprite"
+
+func _ready() -> void:
+	anim.play("idle");
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -9,6 +13,9 @@ func _process(_delta: float) -> void:
 		if Input.is_action_just_pressed("Add"):
 			GameManager.player_inventory = null;
 			GameManager.player_inventory_sprite = null;
+			anim.play("burning");
+			await get_tree().create_timer(2.0).timeout;
+			anim.play("idle");
 
 func body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
