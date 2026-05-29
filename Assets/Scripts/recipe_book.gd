d9bafb3e63b140d7027e18a697619f399ccd4688
Kay Faraday <yatagarasu163@gmail.com>
2026-05-29 19:12:06 +0800

    Revert "Merge branch 'Patients-Symptoms' of https://github.com/Yatagarasu163/apothecary-allergies into Patients-Symptoms"
    This reverts commit 87bbf85c84db1d946063f5e22ee3caac8481c8c1, reversing
    changes made to 918a61535d0d103dc0413adacc41a4974cdc3ef9.

--- Assets/Scripts/recipe_book.gd
+++ Assets/Scripts/recipe_book.gd
@@ -5,10 +5,7 @@ extends Node2D
 
 @export var recipeUI: Node
 
-@export var audio: AudioStreamPlayer
-
 var is_player_inside: bool = false;
-var is_reading: bool = false
 var newPage: int
 
 func _ready() -> void:
@@ -22,15 +19,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if is_reading:
-		if Input.is_action_just_pressed("Add"):
-			_on_prevous_page_pressed()
-		if Input.is_action_just_pressed("Interact"):
-			_on_next_page_pressed()
-	if is_player_inside && Input.is_action_just_pressed("Add") and not is_reading:
-		audio.pitch_scale = randf_range(0.5, 2.0);
-		audio.play()
-		is_reading = true
+	if is_player_inside && Input.is_action_just_pressed("Add"):
 		for page in recipePages:
 			page.visible = false;
 		recipePages[4].visible = true;
@@ -39,7 +28,6 @@ func _process(_delta: float) -> void:
 		recipeUI.visible = !recipeUI.visible;
 	if is_player_inside == false:
 		recipeUI.visible = false;
-		is_reading = false
 		
 
 func updatePage():
@@ -58,15 +46,11 @@ func body_exited(body: Node2D) -> void:
 
 func _on_next_page_pressed() -> void:
 	if (currentPage + 1 > recipePages.size() - 1): return
-	audio.pitch_scale = randf_range(0.5, 2.0);
-	audio.play()
 	newPage = currentPage + 1
 	updatePage()
 
 
 func _on_prevous_page_pressed() -> void:
-	audio.pitch_scale = randf_range(0.5, 2.0);
-	audio.play()
 	if (currentPage - 1 < 0): return
 	newPage = currentPage - 1
 	updatePage()
