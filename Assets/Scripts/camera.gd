746929fcf17da1aed8d3f78d7fc1f7ec6893d613
BarrelPanda <samuel.leong.2006@gmail.com>
2026-05-29 23:41:37 +0800

    update

--- Assets/Scripts/camera.gd
+++ Assets/Scripts/camera.gd
@@ -2,7 +2,11 @@ extends Camera2D
 class_name CameraControl
 
 @export var camMoveSpeed: float = 15
+@export var curtainSpeed: float = 5
+@export var openCurtainY: float = -1050
+
 @onready var current_item_gui = $"CanvasLayer/Control/ItemBoxIcon/Item Icon";
+@onready var curtain = $"CanvasLayer/Control/UI Banner"
 @onready var coin_label = $"CanvasLayer/Coin Score/Coin Count";
 @onready var coin_anim = $"CanvasLayer/Coin Score/Coin";
 @onready var canvas_layer = $CanvasLayer2;
@@ -27,6 +31,13 @@ func _process(_delta: float) -> void:
 		current_item_gui.texture = GameManager.player_inventory_sprite;
 		current_item_gui.visible = true;
 	position = lerp(position, currentTargetPosition, camMoveSpeed * _delta);
+	
+	if GameManager.nextDaying:
+		curtain.position.y = lerp(curtain.position.y, 0.0, curtainSpeed / 10)
+		currentTargetPosition = Vector2(0,-65)
+	else: 
+		curtain.position.y = lerp(curtain.position.y, openCurtainY, curtainSpeed / 10)
+	
 	coin_label.text = str(GameManager.player_score);
 
 func setNewPosition(newPosition: Vector2):
